3c7144ce58e999debf66abe1b1c77c418dcbfd55
nasti <anastasiasaienko634@gmail.com>
2025-04-12 09:40:39 +0200

    uodate all

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "../src/components/App/App";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
+import { store } from "./redux/store";
+
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
